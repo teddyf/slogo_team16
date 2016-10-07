43d12d067d5c38f4563bd9c260e3f43b4620b745
Teddy Franceschi <ttf6@duke.edu>
2016-10-07 01:51:30 -0400

    Built basic packages that will be implemented.  Started working on the barebones of some classes.

---
+++ src/Parsing/Dictionary.java
@@ -0,0 +1,27 @@
+package Parsing;
+
+import Animals.*;
+import java.util.*;
+import java.lang.reflect.*;
+
+/**
+ * 
+ * @author theodorefranceschi
+ *
+ */
+public class Dictionary {
+    private Turtle turtle;
+    private HashMap<String,Method> methods;
+    
+    public Dictionary(){
+        this.turtle = new Turtle(0,0);
+        populateMethods(turtle);
+    }
+    
+    private void populateMethods(Object a){
+        Method[] methods = a.getClass().getMethods();
+        for(int i = 0; i < methods.length; i++){
+            this.methods.put("temporary",methods[i] );
+        }
+    }
+}
