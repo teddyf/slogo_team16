ad0b02ddba23eff8ba02c7013e2b9562348a832d
Teddy Franceschi <theodore.franceschi@duke.edu>
2016-10-23 02:59:21 -0400

    Upload new file

---
+++ src/Parsing/TreeNode.java
@@ -0,0 +1,35 @@
+package Parsing;
+import java.util.*;
+public class TreeNode {
+    
+    public Expression expression;
+    public ArrayList<TreeNode> neighbors;
+    public TreeNode parent;
+    
+    public TreeNode(){
+        this.neighbors = new ArrayList<TreeNode>();
+    }
+    public TreeNode(Expression expression, TreeNode parent){
+        this.parent = parent;
+        this.expression = expression;
+        this.neighbors = new ArrayList<TreeNode>();
+        parent.push(this);
+    }
+    
+    public void push(TreeNode a){
+        this.neighbors.add(a);
+    }
+    
+    public ArrayList<TreeNode> getNeighbors(){
+        return this.neighbors;
+    }
+    
+    public TreeNode getParent(){
+        return parent;
+    }
+    
+    public String toString(){
+        return this.expression.toString();
+    }
+    
+}
