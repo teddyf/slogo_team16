beaffb80f7eae5171d9cc450f8ba00c2713a7b41
aninda.manocha <aninda.manocha@duke.edu>
2016-11-08 04:10:19 -0500

    checked all files that are out of sync with github

--- src/Parsing/expression/ConstantExpression.java
+++ src/Parsing/expression/ConstantExpression.java
@@ -1,3 +1,8 @@
+package Parsing.expression;
+
+import Parsing.TreeNode;
+import model.animal.Animal;
+
 /**
  * This is the ConstantExpression class, which extends the expression class and is used to process constants that are entered as
  * parameters
@@ -5,11 +10,6 @@
  * @author Teddy Franceschi
  * @author Aninda Manocha
  */
-
-package Parsing.expression;
-
-import Parsing.TreeNode;
-import model.animal.Animal;
 
 public class ConstantExpression extends Expression{
 
