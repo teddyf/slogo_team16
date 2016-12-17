ea1f48b5a995b05c5a032e73a2040dd85bdb2c91
Lucy Zhang <lz107@duke.edu>
2016-12-17 15:17:57 -0500

    Changed animalclick

--- src/View/AnimalClick.java
+++ src/View/AnimalClick.java
@@ -1,8 +1,13 @@
 package View;
 
+import java.io.File;
 import java.util.List;
 
+import Controller.DataSetup.DataSetup;
 import View.helper.Graphics;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import model.animal.Animal;
 
 /**
@@ -17,6 +22,11 @@ public class AnimalClick {
 	private String WEIRDTURTLE = "http://images.clipartpanda.com/turtle-clip-art-turtle6.png";
 
 	private String DEADTURTLE = "http://vignette4.wikia.nocookie.net/penguindrum/images/e/ee/Mawaru_penguindrum_peng_head_by_gintabro-d3lj8vt.png/revision/latest?cb=20120304141806";
+
+	private String fileName;
+	private DataSetup data;
+	private final File INITIAL_DIRECTORY = new File("data");
+	private final File IMAGE_DIRECTORY = new File("Resources");
 
 	private Graphics graphic = new Graphics();
 	private List<Animal> activeTurtles;
@@ -63,11 +73,17 @@ public class AnimalClick {
 	private void updateAnimal(Animal animal) {
 		if (animal.getSelected()) { // it's already active, so deactivate it
 			animal.setSelected(false);
-			//animal.getImageView().setStyle("-fx-image: url(\"" + DEADTURTLE + "\");");
+			// animal.getImageView().setStyle("-fx-image: url(\"" + DEADTURTLE +
+			// "\");");
 		} else {
 			animal.setSelected(true);
-			animal.getImageView().setStyle("-fx-image: url(\"" + WEIRDTURTLE + "\");");
+			createFileDirectory(IMAGE_DIRECTORY, animal);
 		}
+	}
+
+	private void updateAnimalImage(Animal animal, String fileName) {
+		ImageView image = graphic.createImageView(graphic.createImage(fileName));
+		animal.setImageView(image);
 	}
 
 	public List<Animal> getActiveTurtles() {
@@ -87,4 +103,17 @@ public class AnimalClick {
 	public boolean isActiveAnimal(Animal animal) {
 		return (activeTurtles.contains(animal));
 	}
+
+	private void createFileDirectory(File directory, Animal animal) {
+		FileChooser chooser = new FileChooser();
+		chooser.setInitialDirectory(directory);
+		chooser.setTitle("Choose an image");
+		File temp = chooser.showOpenDialog(new Stage());
+		if (temp != null & directory.equals(IMAGE_DIRECTORY)) {
+			fileName = temp.toString();
+			data = new DataSetup(fileName);
+			updateAnimalImage(animal, fileName);
+		}
+	}
+
 }
